318e2154ba7cf6e96e7caedb95451a5a5079583d
Sarah Leder <Trinity16201@gmail.com>
2023-11-30 19:31:21 -0500

    Updated files and tested routes. Corrected errors

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,31 +1,21 @@
-// import models
 const Product = require('./product');
 const Category = require('./category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const User = require('./user');
 
-// Products belongsTo Category
+// User.hasMany(Product, {
+//     foreignKey:"user_id",
+// });
 Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
-// Categories have many Products
+
 Category.hasMany(Product, {
   foreignKey: "category_id",
 });
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: "product_id",
-});
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: "tag_id",
-});
+
 
 module.exports = {
   Product,
   Category,
-  Tag,
-  ProductTag,
+  User
 };
--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,31 +1,9 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const router = require('express').Router();
+const apiRoutes = require('./api');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: "category_id",
-});
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: "category_id",
-});
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: "product_id",
-});
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: "tag_id",
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>")
 });
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
