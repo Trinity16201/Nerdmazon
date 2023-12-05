98a48c0398cd80fa65f428e1daa55beb7dc74a0d
Chia <yangchai2450@gmail.com>
2023-12-04 20:36:11 -0500

    change the html

--- Develop/tailwind.config.js
+++ Develop/tailwind.config.js
@@ -4,10 +4,10 @@ module.exports = {
   theme: {
     screens: {
       screens: {
-        sm: '480px',
+        sm: '640px',
         md: '768px',
-        lg: '976px',
-        xl: '1440px',
+        lg: '1024px',
+        xl: '1280px',
     },
     extend: {
       colors: {
