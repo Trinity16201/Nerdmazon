53e19edd272c2611799a923aa7ebaeeee2509f37
Chia <yangchai2450@gmail.com>
2023-12-04 21:24:43 -0500

    added homeroutes

--- Develop/routes/api/user-routes.js
+++ Develop/routes/api/user-routes.js
@@ -2,89 +2,81 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../../models');
 
-/* //Route to get all users
-router.get('/users', (req, res) =>{
-    User.findAll({
-        include: [{model: User}]
-    }).then(user => res.json(user))
-    .catch(e => res.status(400).json(e));
-});
-
-//Route to get user by id
-router.get('/users/:id', (req, res) => {
-    // find a single product by its `id`
-    User.findOne({
-      where: { id:req.params.id },
-      include: [{ model:User }]
-    })
-    .then(user => res.json(user))
-    .catch(e => res.status(400).json(e));
-    
-  }); */
-
-//Route to create a new user
-router.post('/signup', (req, res) => {
-    const {name,email,password} = req.body;
-    User.create({name,email,password})
-    .then(newUser => {
-        res.status(200).json(newUser);
-    })
-    .catch(e => {
-        console.error(e);
-        res.status(500).json({error:"Unable to create new user"});
-    });
-});
-
-//Route to update a users info
-router.put('/users/:id', (req, res) => {
-    const {id} = req.params;
-    const {name, email, password } = req.body
-    User.update({name, email, password}, {where:{id} })
-        .then(updatedInfo => {
-            if (updatedInfo[0] === 0){
-                return res.status(400).json({error: "Unable to find user"});
-            }
-            res.status(200).json({message: "Updated user successfully"});
-        }).catch(e => {
-            console.error(e);
-            res.status(500).json({error: "Unable to update user"})
-        });
-});
-
-//Route to delete a user
-router.delete('/delete/:id', (req, res) => {
-    const {id} = req.params;
-    User.destory({where: {id}})
-        .then(deletedUser =>{
-            if(deletedUser === 0){
-                return res.status(400).json({error: "Unable to find user"})
-            }
-            res.status(200).json({message: "Deleted user successfully"})
-        })
-        .catch(e =>{
-            console.error(e);
-            res.status(500).json({error: "Unable to delete user"});
-        });
-});
-
-//Route to login
-router.post('/login', (req, res) => {
-    const {email,password} = req.body;
-    User.findOne({where: {email}})
-    .then(user => {
-        if(!user){
-            return res.status(400).json({error: "Unable to find user"});
-        }
-
-        if(!user.checkPassword(password)){
-            return res.status(400).json({error: "Incorrect password"});
-        }
-
-        res.status(200).json({message:"Login successful"})
-    })
-    .catch(e => {
-        console.error(e);
-        res.status(500).json({error: "Login unsuccessful"});
-    })
-})
-module.exports = router;
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const projectData = await Project.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const projects = projectData.map((project) => project.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        projects, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/project/:id', async (req, res) => {
+    try {
+      const projectData = await Project.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const project = projectData.get({ plain: true });
+  
+      res.render('project', {
+        ...project,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  // Use withAuth middleware to prevent access to route
+  router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Project }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
