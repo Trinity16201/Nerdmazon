e358a103210b84dea569533fe96e1550be052c91
Chia <yangchai2450@gmail.com>
2023-12-05 19:08:52 -0500

    heroku commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./test/**/*.html'],
   theme: {
@@ -9,7 +11,18 @@ module.exports = {
         lg: '1024px',
         xl: '1280px',
     },
-
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+      }
+    },
   },
   plugins: [],
 }
